43d11655e5846f7e9f17dbc3b66e5d55b02a2e9b
Nathan Johnson <drugen@gmail.com>
2020-11-21 16:53:44 -0700

    hi

--- system/asgi.py
+++ system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for lifemanagement_webapp project.
+ASGI config for system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lifemanagement_webapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'system.settings')
 
 application = get_asgi_application()
