ea422d858b60ee32be958d15163a45d59cc88783
Nathan Johnson <drugen@gmail.com>
2021-01-03 21:14:41 -0700

    style and mixin work.

--- app/core/mixins.py
+++ app/core/mixins.py
@@ -9,7 +9,13 @@ class SecurityMixin(models.Model):
 
 
 class ArchiveMixin(models.Model):
-    pass
+    is_archived = models.BooleanField(default=False)
+
+    def archive(self):
+        self.is_archived = True
+
+    def un_archive(self):
+        self.is_archived = False
 
     class Meta:
         abstract = True
