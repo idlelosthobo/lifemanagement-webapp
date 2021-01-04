626de1a2dc57d3ed4b00182ec4a5584a8f77435e
Nathan <drugen@gmail.com>
2021-01-03 23:59:38 -0700

    mixin update no more database app may fail.

--- app/core/mixins.py
+++ app/core/mixins.py
@@ -1,23 +1,67 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.contenttypes.fields import GenericRelation
+from app.core.models import Security, Statistic
 
 
 class SecurityMixin(models.Model):
-    pass
+    security = GenericRelation(Security, related_name='securities', related_query_name='security')
+
+    def update(self, user):
+        try:
+            security = Security.objects.get(content_type__model=self, object_id=self.pk, user=user)
+        except Statistic.DoesNotExist:
+            security = None
+        if security:
+            security.user = user
+            security.save()
+        else:
+            self.security.create(view_count=1, user=user)
 
     class Meta:
         abstract = True
 
 
-class ArchiveMixin(models.Model):
-    is_archived = models.BooleanField(default=False)
+class HistoryMixin(models.Model):
+    is_archived = models.BooleanField(default=False, editable=False)
+    is_to_be_deleted = models.BooleanField(default=False, editable=False)
+    deletion_date = models.DateField(auto_now_add=True, editable=False)
 
-    def archive(self):
+    def set_to_be_deleted(self):
+        self.is_to_be_deleted = True
+        self.save()
+
+    def un_set_to_be_deleted(self):
+        self.is_to_be_deleted = False
+        self.save()
+
+    def set_archived(self):
         self.is_archived = True
+        self.save()
 
-    def un_archive(self):
+    def un_set_archived(self):
         self.is_archived = False
+        self.save()
 
     class Meta:
         abstract = True
 
 
+class StatisticMixin(models.Model):
+    date_entered = models.DateTimeField(auto_now_add=True, editable=False)
+    date_updated = models.DateTimeField(auto_now=True, editable=False)
+    statistic = GenericRelation(Statistic, related_name='statistics', related_query_name='statistic', editable=False)
+
+    def update(self, user):
+        try:
+            statistic = Statistic.objects.get(content_type__model=self, object_id=self.pk, user=user)
+        except Statistic.DoesNotExist:
+            statistic = None
+        if statistic:
+            statistic.view_count += 1
+            statistic.save()
+        else:
+            self.statistic.create(view_count=1, user=user)
+
+    class Meta:
+        abstract = True
--- app/core/models.py
+++ app/core/models.py
@@ -5,3 +5,18 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 
+class Security(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, editable=False)
+    object_id = models.PositiveIntegerField(editable=False)
+    content_object = GenericForeignKey('content_type', 'object_id')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+
+
+class Statistic(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, editable=False)
+    object_id = models.PositiveIntegerField(editable=False)
+    content_object = GenericForeignKey('content_type', 'object_id')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+
+    view_count = models.PositiveIntegerField(default=0, editable=False)
+
